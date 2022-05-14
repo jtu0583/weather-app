702cb6eeb53ab6fb9a07a186cbf52a94ef344eab
jtu0583 <100534944+jtu0583@users.noreply.github.com>
2022-05-14 10:32:08 -0400

    added the relevant icon/description from API

--- src/app.js
+++ src/app.js
@@ -32,6 +32,15 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#weather-icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
